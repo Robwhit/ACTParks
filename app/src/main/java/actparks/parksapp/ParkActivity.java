df51d5420c347334a17da392dddd8dd14dedd0c9
XinyuHou <u5916376@anu.edu.au>
2018-08-16 20:10:53 +1000

    finished the 'Park Name' page (2tabs)

--- app/src/main/java/actparks/parksapp/ParkActivity.java
+++ app/src/main/java/actparks/parksapp/ParkActivity.java
@@ -2,6 +2,7 @@ package actparks.parksapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class ParkActivity extends AppCompatActivity {
 
@@ -10,6 +11,22 @@ public class ParkActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_park);
+
+        // Create Tabs
+        TabHost host = (TabHost)findViewById(R.id.tabHostParks);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Information");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Information");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Map");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Map");
+        host.addTab(spec);
 
 
     }
