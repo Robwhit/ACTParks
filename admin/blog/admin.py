cd943b7581056cd2eac31e4e220f4868cdb8b3f6
Zijie Pan <>
2018-08-25 22:49:47 +1000

    backend works

---
+++ admin/blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Parkinformation
+from .models import Pathinformation
+
+admin.site.register(Parkinformation)
+
+admin.site.register(Pathinformation)
