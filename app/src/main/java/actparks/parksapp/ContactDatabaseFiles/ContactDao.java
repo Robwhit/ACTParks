7e29677ae9bb50638538f9b1b2f91736e788f45f
XinyuHou <u5916376@anu.edu.au>
2018-08-26 15:16:18 +1000

    finished the contact page, contact database, and show the information

---
+++ app/src/main/java/actparks/parksapp/ContactDatabaseFiles/ContactDao.java
@@ -0,0 +1,21 @@
+package actparks.parksapp.ContactDatabaseFiles;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Contact contact);
+
+    @Query("DELETE FROM contact_table")
+    void deleteAll();
+
+    @Query("SELECT * from contact_table ORDER BY contactId ASC")
+    LiveData<List<Contact>> getAllContacts();
+}
---
+++ app/src/main/java/actparks/parksapp/ContactDatabaseFiles/ContactRepository.java
@@ -0,0 +1,41 @@
+package actparks.parksapp.ContactDatabaseFiles;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class ContactRepository {
+    private ContactDao mContactDao;
+    private LiveData<List<Contact>> mAllContacts;
+
+    ContactRepository(Application application){
+        ContactRoomDatabase db = ContactRoomDatabase.getDatabase(application);
+        mContactDao = db.contactDao();
+        mAllContacts = mContactDao.getAllContacts();
+    }
+
+    LiveData<List<Contact>> getmAllContacts() {
+        return mAllContacts;
+    }
+
+    public void insert(Contact contact){
+        new ContactRepository.insertAsyncTask(mContactDao).execute(contact);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Contact, Void, Void> {
+
+        private ContactDao mAsyncTaskDao;
+
+        insertAsyncTask(ContactDao dao){
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Contact... params){
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
