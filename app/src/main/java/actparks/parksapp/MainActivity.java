4d992caea7ac338ba8311a89d6eb0459459ff31b
jingjinggit config --global user.email 745287546@qq.com <>
2018-08-28 02:01:16 +1000

    slightly change

--- app/src/main/java/actparks/parksapp/MainActivity.java
+++ app/src/main/java/actparks/parksapp/MainActivity.java
@@ -1,31 +1,19 @@
 package actparks.parksapp;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.app.FragmentManager;
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
-import android.arch.persistence.db.SupportSQLiteDatabase;
-import android.arch.persistence.room.RoomDatabase;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -34,19 +22,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import java.util.List;
-
-import actparks.parksapp.ParkDatabaseFiles.*;
-
-import actparks.parksapp.WalkDatabaseFiles.Walk;
-import actparks.parksapp.WalkDatabaseFiles.WalkDao;
-import actparks.parksapp.WalkDatabaseFiles.WalkListAdapter;
-import actparks.parksapp.WalkDatabaseFiles.WalkRoomDatabase;
-import actparks.parksapp.WalkDatabaseFiles.WalkViewModel;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
