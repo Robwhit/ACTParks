aea176c9dbbf9e04837b3a8f2dfc7d0ae1af3451
Zijie Pan <>
2018-08-31 13:06:13 +1000

    update coordiantes

--- admin/blog/models.py
+++ admin/blog/models.py
@@ -4,7 +4,9 @@ from django.utils import timezone
 
 class Parkinformation(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    park_name = models.CharField(max_length=200)
+    X_coordinate = models.CharField(max_length=7, default=0)
+    Y_coordinate = models.CharField(max_length=7, default=0)
     Description = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -16,11 +18,11 @@ class Parkinformation(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.park_name
 
 class Pathinformation(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    path_name = models.CharField(max_length=200)
     Description = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -32,4 +34,4 @@ class Pathinformation(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.path_name
