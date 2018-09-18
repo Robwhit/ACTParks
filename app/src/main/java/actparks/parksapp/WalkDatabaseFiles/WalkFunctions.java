f0e1011a3530853e747b50b336635aee732542ca
Rob <u5589395@anu.edu.au>
2018-09-18 17:44:46 +1000

    Created walk functions class

---
+++ app/src/main/java/actparks/parksapp/WalkDatabaseFiles/WalkFunctions.java
@@ -0,0 +1,19 @@
+package actparks.parksapp.WalkDatabaseFiles;
+
+import android.graphics.Point;
+import android.location.Location;
+
+import java.util.ArrayList;
+
+public class WalkFunctions {
+
+    // list of coordinates for map
+    ArrayList<Point> coordinateList;
+    ArrayList<Float> heightList;
+
+    // TODO: Recieve the route from the database and create ArrayLists from it;
+    public void getRoute(int walkId){
+
+    }
+
+}
