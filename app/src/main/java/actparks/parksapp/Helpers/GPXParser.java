36c0caa8cae6fe525751c42a88d8c7756f689e1e
jingjinggit config --global user.email 745287546@qq.com <>
2018-09-25 21:16:55 +1000

    extract lat and lon from gpx file

---
+++ app/src/main/java/actparks/parksapp/Helpers/GPXParser.java
@@ -0,0 +1,79 @@
+package actparks.parksapp.Helpers;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class GPXParser {
+
+    public List<List> ConvertGPX(File file){
+        List<List> list = new ArrayList<>();
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+        int ord = 0;
+        try{
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            FileInputStream fileInputStream = new FileInputStream(file);
+            Document document = documentBuilder.parse(fileInputStream);
+            Element elementRoot = document.getDocumentElement();
+
+            NodeList nodelist_trkpt = elementRoot.getElementsByTagName("trkpt");
+
+            for(int i  = 0; i < nodelist_trkpt.getLength(); i++){
+
+                ArrayList single_list = new ArrayList();
+
+
+                Node node = nodelist_trkpt.item(i);
+
+                NamedNodeMap attributes = node.getAttributes();
+
+                String newLatitude = attributes.getNamedItem("lat").getTextContent();
+
+                String newLongitude = attributes.getNamedItem("lon").getTextContent();
+
+                String newLocationName = newLatitude + ":" + newLongitude;
+
+                single_list.add(newLocationName);
+
+                list.add(single_list);
+            }
+            fileInputStream.close();
+
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        GPXParser gpxParser = new GPXParser();
+        List<List>list = new ArrayList<>();
+        String path = "/Users/Jingjing//Downloads/Afternoon_Run.gpx";
+        File file = new File(path);
+        list = gpxParser.ConvertGPX(file);
+        System.out.print(list);
+    }
+}
+
