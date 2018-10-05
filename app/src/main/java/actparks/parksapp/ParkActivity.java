882ece8c108f6404c52c4a7908576886b776762c
XinyuHou <u5916376@anu.edu.au>
2018-10-06 02:20:15 +1000

    add current location to Park activity's map

--- app/src/main/java/actparks/parksapp/ParkActivity.java
+++ app/src/main/java/actparks/parksapp/ParkActivity.java
@@ -1,21 +1,43 @@
 package actparks.parksapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.location.Location;
+import android.widget.Toast;
 
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
+
+import java.util.List;
 
 import actparks.parksapp.ParkDatabaseFiles.Park;
 
-public class ParkActivity extends AppCompatActivity {
+public class ParkActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener {
     TextView title;
     private MapView mapView;
+    private MapboxMap mapboxMap;
+    private PermissionsManager permissionsManager;
+    private LocationLayerPlugin locationLayerPlugin;
+    private LocationEngine locationEngine;
+    private Location originLocation;
+    private LatLng originCoord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,41 +86,118 @@ public class ParkActivity extends AppCompatActivity {
             //map
             mapView = findViewById(R.id.mapParkView);
             mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(this);
         } else {
             // ...
         }
 
+        //map
+//        mapView.getMapAsync(new OnMapReadyCallback() {
+//            @Override
+//            public void onMapReady(MapboxMap mapboxMap) {
+//                ParkActivity.this.mapbM = mapboxMap;
+//                enableLocationPlugin();
+//                System.out.println("121231231231231231231231231: "+locationEngine.toString());
+//            }
+//        });
+//        originCoord = new LatLng(originLocation.getLatitude(), originLocation.getLongitude());
+//        mapboxMap.addOnMapClickListener(this);
     }
 
     //map
     @Override
-    public void onStart() {
-        super.onStart();
-        mapView.onStart();
+    public void onMapReady(MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        enableLocationPlugin();
+        originCoord = new LatLng(originLocation.getLatitude(), originLocation.getLongitude());
+        System.out.println("11111: "+originCoord.getLatitude() + " : " + originCoord.getLongitude());
+//        mapboxMap.addOnMapClickListener(this);
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationPlugin() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            initializeLocationEngine();
+            LocationLayerPlugin locationLayerPlugin = new LocationLayerPlugin(mapView, mapboxMap);
+            locationLayerPlugin.setCameraMode( CameraMode.TRACKING);
+            getLifecycle().addObserver(locationLayerPlugin);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void initializeLocationEngine() {
+        LocationEngineProvider locationEngineProvider = new LocationEngineProvider(this);
+        locationEngine = locationEngineProvider.obtainBestLocationEngineAvailable();
+        locationEngine.setPriority( LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+
+        Location lastLocation = locationEngine.getLastLocation();
+        if (lastLocation != null) {
+            originLocation = lastLocation;
+        } else {
+            locationEngine.addLocationEngineListener(this);
+        }
     }
 
     @Override
-    public void onResume() {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+//        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enableLocationPlugin();
+            System.out.println("abcdefghijklmn");
+        } else {
+//            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+        if (locationLayerPlugin != null) {
+            locationLayerPlugin.onStart();
+        }
+    }
+
+    @Override
+    protected void onResume() {
         super.onResume();
         mapView.onResume();
     }
 
     @Override
-    public void onPause() {
+    protected void onPause() {
         super.onPause();
         mapView.onPause();
     }
 
     @Override
-    public void onStop() {
+    protected void onStop() {
         super.onStop();
         mapView.onStop();
+        if (locationLayerPlugin != null) {
+            locationLayerPlugin.onStart();
+        }
     }
 
     @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 
     @Override
@@ -108,9 +207,19 @@ public class ParkActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onConnected() {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 
     //
