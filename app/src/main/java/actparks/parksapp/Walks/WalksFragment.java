7b0c474d5c4dad018f42ddd2ab94b9d7190262c3
XinyuHou <u5916376@anu.edu.au>
2018-10-06 03:39:04 +1000

    fix suppressLint

--- app/src/main/java/actparks/parksapp/Walks/WalksFragment.java
+++ app/src/main/java/actparks/parksapp/Walks/WalksFragment.java
@@ -65,87 +65,88 @@ public class WalksFragment extends Fragment {
 
         return myView;
     }
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        super.onViewCreated( view, savedInstanceState );
         // Walks Recycler View
         // Recycler View
-        RecyclerView walkrecyclerView = (RecyclerView) myView.findViewById(R.id.walksrecyclerview);
-        final WalkListAdapter adapter = new WalkListAdapter(getActivity());
-        adapter.setOnItemClickListener(new WalkClickListener() {
+        RecyclerView walkrecyclerView = (RecyclerView) myView.findViewById( R.id.walksrecyclerview );
+        final WalkListAdapter adapter = new WalkListAdapter( getActivity() );
+        adapter.setOnItemClickListener( new WalkClickListener() {
             @Override
             public void onWalkClick(int position, View v) {
 
-                Intent intent = new Intent(getActivity(), WalksActivity.class);
+                Intent intent = new Intent( getActivity(), WalksActivity.class );
                 //TODO: Make change position
-                Walk walk = adapter.getWalk(position);
-                String name = adapter.getWalkName(position);
-                intent.putExtra("Walk", walk);
+                Walk walk = adapter.getWalk( position );
+                String name = adapter.getWalkName( position );
+                intent.putExtra( "Walk", walk );
 
-                startActivity(intent);
+                startActivity( intent );
             }
-        });
+        } );
         final WalkListAdapter adapter1 = adapter;
-        walkrecyclerView.setAdapter(adapter);
-        walkrecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        walkrecyclerView.setAdapter( adapter );
+        walkrecyclerView.setLayoutManager( new LinearLayoutManager( getActivity() ) );
 
         // Walks
-        mWalkViewModel = ViewModelProviders.of(this).get(WalkViewModel.class);
+        mWalkViewModel = ViewModelProviders.of( this ).get( WalkViewModel.class );
 
         // initial order
-        mWalkViewModel.getmAllWalks().observe(this, new Observer<List<Walk>>() {
+        mWalkViewModel.getmAllWalks().observe( this, new Observer<List<Walk>>() {
             @Override
             public void onChanged(@Nullable final List<Walk> walks) {
                 // Update the cached copy of the words in the adapter.
-                adapter1.setWalks(walks);
+                adapter1.setWalks( walks );
             }
-        });
+        } );
 
         //search
-        mySearchView = (SearchView) myView.findViewById(R.id.search_bar);
+        mySearchView = (SearchView) myView.findViewById( R.id.search_bar );
 
-        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        mySearchView.setOnQueryTextListener( new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                System.out.println( "TextSubmit : " + s);
-                mWalkViewModel.searchWalkName(s).observe(WalksFragment.this, new Observer<List<Walk>>() {
+                System.out.println( "TextSubmit : " + s );
+                mWalkViewModel.searchWalkName( s ).observe( WalksFragment.this, new Observer<List<Walk>>() {
                     @Override
                     public void onChanged(@Nullable final List<Walk> walks) {
                         // Update the cached copy of the words in the adapter.
-                        adapter1.setWalks(walks);
+                        adapter1.setWalks( walks );
                     }
-                });
+                } );
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
-                System.out.println( "TextChange : " + s);
-                mWalkViewModel.searchWalkName(s).observe(WalksFragment.this, new Observer<List<Walk>>() {
+                System.out.println( "TextChange : " + s );
+                mWalkViewModel.searchWalkName( s ).observe( WalksFragment.this, new Observer<List<Walk>>() {
                     @Override
                     public void onChanged(@Nullable final List<Walk> walks) {
                         // Update the cached copy of the words in the adapter.
-                        adapter1.setWalks(walks);
+                        adapter1.setWalks( walks );
                     }
-                });
+                } );
                 return false;
             }
-        });
+        } );
 
 
         //Filter
-        button_filter = (Button) myView.findViewById(R.id.walks_filter_button);
-        button_filter.setOnClickListener(new View.OnClickListener() {
+        button_filter = (Button) myView.findViewById( R.id.walks_filter_button );
+        button_filter.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Creating the instance of PopupMenu
-                PopupMenu popup = new PopupMenu(getContext(), button_filter);
+                PopupMenu popup = new PopupMenu( getContext(), button_filter );
                 //Inflating the Popup using xml file
                 popup.getMenuInflater()
-                        .inflate(R.menu.filter_walks, popup.getMenu());
+                        .inflate( R.menu.filter_walks, popup.getMenu() );
 
                 //registering popup with OnMenuItemClickListener
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                popup.setOnMenuItemClickListener( new PopupMenu.OnMenuItemClickListener() {
                     public boolean onMenuItemClick(MenuItem item) {
                         Toast.makeText(
                                 getContext(),
@@ -153,52 +154,49 @@ public class WalksFragment extends Fragment {
                                 Toast.LENGTH_SHORT
                         ).show();
 
-                        if (item.getTitle().equals("filter by distance")){
+                        if (item.getTitle().equals( "filter by distance" )) {
                             // alert window to get user's input filter min_distance and max_distance
                             // get xml view
-                            LayoutInflater lif = LayoutInflater.from(getContext());
-                            View alertView = lif.inflate(R.layout.filter_alert, null);
-                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+                            LayoutInflater lif = LayoutInflater.from( getContext() );
+                            View alertView = lif.inflate( R.layout.filter_alert, null );
+                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder( getContext() );
 
-                            alertDialogBuilder.setView(alertView);
+                            alertDialogBuilder.setView( alertView );
 
                             final EditText min_distance = (EditText) alertView
-                                    .findViewById(R.id.min_distance);
+                                    .findViewById( R.id.min_distance );
                             final EditText max_distance = (EditText) alertView
-                                    .findViewById(R.id.max_distance);
+                                    .findViewById( R.id.max_distance );
 
                             // set dialog message
                             alertDialogBuilder
-                                    .setCancelable(false)
-                                    .setPositiveButton("APPLY",
+                                    .setCancelable( false )
+                                    .setPositiveButton( "APPLY",
                                             new DialogInterface.OnClickListener() {
-                                                public void onClick(DialogInterface dialog,int id) {
+                                                public void onClick(DialogInterface dialog, int id) {
                                                     // check if both min and max are not empty
-                                                    if(min_distance.getText().toString().equals("") || max_distance.getText().toString().equals(""))
-                                                    {
-                                                        Toast.makeText(getContext(), "Input distance Is Empty, Please Enter Some Text", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                    else
-                                                    {
+                                                    if (min_distance.getText().toString().equals( "" ) || max_distance.getText().toString().equals( "" )) {
+                                                        Toast.makeText( getContext(), "Input distance Is Empty, Please Enter Some Text", Toast.LENGTH_SHORT ).show();
+                                                    } else {
                                                         float min = Float.valueOf( min_distance.getText().toString() );
                                                         float max = Float.valueOf( max_distance.getText().toString() );
-                                                        mWalkViewModel.filterByDistance(min, max).observe(WalksFragment.this, new Observer<List<Walk>>() {
+                                                        mWalkViewModel.filterByDistance( min, max ).observe( WalksFragment.this, new Observer<List<Walk>>() {
                                                             @Override
                                                             public void onChanged(@Nullable final List<Walk> walks) {
                                                                 // Update the cached copy of the words in the adapter.
-                                                                adapter1.setWalks(walks);
+                                                                adapter1.setWalks( walks );
                                                             }
-                                                        });
+                                                        } );
                                                     }
 
                                                 }
-                                            })
-                                    .setNegativeButton("Cancel",
+                                            } )
+                                    .setNegativeButton( "Cancel",
                                             new DialogInterface.OnClickListener() {
-                                                public void onClick(DialogInterface dialog,int id) {
+                                                public void onClick(DialogInterface dialog, int id) {
                                                     dialog.cancel();
                                                 }
-                                            });
+                                            } );
 
                             // create alert dialog
                             AlertDialog alertDialog = alertDialogBuilder.create();
@@ -207,17 +205,17 @@ public class WalksFragment extends Fragment {
                             alertDialog.show();
 
 
-                        }else if(item.getTitle().equals("filter something later")) {
+                        } else if (item.getTitle().equals( "filter something later" )) {
 
                             // TODO: add more filters later
                             // initial order
-                            mWalkViewModel.getmAllWalks().observe(WalksFragment.this, new Observer<List<Walk>>() {
+                            mWalkViewModel.getmAllWalks().observe( WalksFragment.this, new Observer<List<Walk>>() {
                                 @Override
                                 public void onChanged(@Nullable final List<Walk> walks) {
                                     // Update the cached copy of the words in the adapter.
-                                    adapter1.setWalks(walks);
+                                    adapter1.setWalks( walks );
                                 }
-                            });
+                            } );
 
                             Toast.makeText(
                                     getContext(),
@@ -230,22 +228,23 @@ public class WalksFragment extends Fragment {
                         return true;
 
                     }
-                });
+                } );
 
                 popup.show(); //showing popup menu
             }
-        }); //closing the setOnClickListener method
+        } ); //closing the setOnClickListener method
 
 
         //Sort
-        button_sort = (Button) myView.findViewById(R.id.walks_sort_button);
-        button_sort.setOnClickListener(new View.OnClickListener() {
+        button_sort = (Button) myView.findViewById( R.id.walks_sort_button );
+        button_sort.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(getContext(), button_sort);
+                PopupMenu popup = new PopupMenu( getContext(), button_sort );
                 popup.getMenuInflater()
-                        .inflate(R.menu.sort_walks, popup.getMenu());
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        .inflate( R.menu.sort_walks, popup.getMenu() );
+                popup.setOnMenuItemClickListener( new PopupMenu.OnMenuItemClickListener() {
+                    @SuppressLint("MissingPermission")
                     public boolean onMenuItemClick(MenuItem item) {
                         Toast.makeText(
                                 getContext(),
@@ -253,30 +252,30 @@ public class WalksFragment extends Fragment {
                                 Toast.LENGTH_SHORT
                         ).show();
 
-                        if (item.getTitle().equals("sort by name")){
-                            mWalkViewModel.getmAllWalks().observe(WalksFragment.this, new Observer<List<Walk>>() {
+                        if (item.getTitle().equals( "sort by name" )) {
+                            mWalkViewModel.getmAllWalks().observe( WalksFragment.this, new Observer<List<Walk>>() {
                                 @Override
                                 public void onChanged(@Nullable final List<Walk> walks) {
                                     // Update the cached copy of the words in the adapter.
-                                    adapter1.setWalks(walks);
+                                    adapter1.setWalks( walks );
                                 }
-                            });
-                        }else if(item.getTitle().equals("sort by distance")) {
+                            } );
+                        } else if (item.getTitle().equals( "sort by distance" )) {
 
-                            mWalkViewModel.sortByDistance().observe(WalksFragment.this, new Observer<List<Walk>>() {
+                            mWalkViewModel.sortByDistance().observe( WalksFragment.this, new Observer<List<Walk>>() {
                                 @Override
                                 public void onChanged(@Nullable final List<Walk> walks) {
                                     // Update the cached copy of the words in the adapter.
-                                    adapter1.setWalks(walks);
+                                    adapter1.setWalks( walks );
                                 }
-                            });
+                            } );
 
-                        }else if(item.getTitle().equals("sort by distance from my location")) {
+                        } else if (item.getTitle().equals( "sort by distance from my location" )) {
 //                            currentLocation =
 //                            System.out.println("");
                             String locationProvider;
                             locationProvider = LocationManager.GPS_PROVIDER;
-                            currentLocation = MainActivity.locationManager.getLastKnownLocation(locationProvider);
+                            currentLocation = MainActivity.locationManager.getLastKnownLocation( locationProvider );
                             System.out.println("laaa: "+currentLocation.getLatitude()+" -- looo: "+currentLocation.getLongitude());
 
                             mWalkViewModel.sortByDistance().observe(WalksFragment.this, new Observer<List<Walk>>() {
